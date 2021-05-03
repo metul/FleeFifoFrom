c57dd9f9aa0a622efcabb764f64906c931f65a60
Anastasia Pomelova <pomelova@in.tum.de>
2021-05-04 00:47:11 +0200

    basic interactions

---
+++ FleeFifoFrom/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void Authorize()
+    {
+        Debug.Log("Player chose the authorize action");
+    }
+
+    public void Swap()
+    {
+        Debug.Log("Player chose the swap action");
+    }
+
+    public void Riot()
+    {
+        Debug.Log("Player chose the riot action");
+    }
+
+    public void Revive()
+    {
+        Debug.Log("Player chose the revive action");
+    }
+
+    public void Objective()
+    {
+        Debug.Log("Player chose the objective action");
+    }
+
+    public void Countermand()
+    {
+        Debug.Log("Player chose the countermand action");
+    }
+
+    public void Reprioritize()
+    {
+        Debug.Log("Player chose the reprioritize action");
+    }
+
+    public void Retreat()
+    {
+        Debug.Log("Player chose the retreat action");
+    }
+
+    public void Recall()
+    {
+        Debug.Log("Player chose the recall action");
+    }
+
+    public void Cooperate()
+    {
+        Debug.Log("Player chose the cooperate action");
+    }
+
+    public void Poach()
+    {
+        Debug.Log("Player chose the poach action");
+    }
+
+    public void Villager()
+    {
+        Debug.Log("Player chose the draw villager action");
+    }
+}
