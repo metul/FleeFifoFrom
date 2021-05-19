bb0d57c35cafd9075ec1853050d469bc22e25f1f
metul <mertulker14@gmail.com>
2021-05-19 17:51:54 +0300

    Automatically start server for headless server build, modify (dis)connect callbacks

--- FleeFifoFrom/Assets/Scripts/Networking/ConnectionManager.cs
+++ FleeFifoFrom/Assets/Scripts/Networking/ConnectionManager.cs
@@ -1,4 +1,5 @@
 using MLAPI;
+using MLAPI.Logging;
 using MLAPI.Transports.UNET;
 using System.Collections;
 using System.Collections.Generic;
@@ -18,14 +19,50 @@ public class ConnectionManager : MonoBehaviour
 
     private void Start()
     {
-        
+#if UNITY_SERVER
+        NetworkManager.Singleton.StartServer();
+#endif
     }
 
     private void Update()
     {
-        // MARK: Used for debug purposes
         if (Input.GetKeyDown(KeyCode.F1))
             _connectionPanel.SetActive(!_connectionPanel.activeSelf);
+    }
+
+    private void OnEnable()
+    {
+        NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
+        NetworkManager.Singleton.OnClientDisconnectCallback += OnClientDisconnected;
+    }
+
+    private void OnDisable()
+    {
+        NetworkManager.Singleton.OnClientConnectedCallback -= OnClientConnected;
+        NetworkManager.Singleton.OnClientDisconnectCallback -= OnClientDisconnected;
+    }
+
+    /// <summary>
+    /// Callback for client connection.
+    /// </summary>
+    /// <param name="obj"> ID for connected client. </param>
+    private void OnClientConnected(ulong obj)
+    {
+        NetworkLog.LogInfoServer($"Client {obj} connected at timestamp {Time.time}.");
+    }
+
+    /// <summary>
+    /// Callback for client disconnection.
+    /// </summary>
+    /// <param name="obj"> ID for disconnected client. </param>
+    private void OnClientDisconnected(ulong obj)
+    {
+        NetworkLog.LogInfoServer($"Client {obj} disconnected at timestamp {Time.time}.");
+    }
+
+    private bool IsValidIPv4(string ipString)
+    {
+        return ipString.Count(c => c == '.') == 3 && IPAddress.TryParse(ipString, out IPAddress iPAddress);
     }
 
     public void StartHost()
@@ -44,10 +81,5 @@ public class ConnectionManager : MonoBehaviour
     {
         if (!string.IsNullOrEmpty(value) && IsValidIPv4(value))
             _transport.ConnectAddress = value;
-    }
-
-    private bool IsValidIPv4(string ipString)
-    {
-        return ipString.Count(c => c == '.') == 3 && IPAddress.TryParse(ipString, out IPAddress iPAddress);
     }
 }
