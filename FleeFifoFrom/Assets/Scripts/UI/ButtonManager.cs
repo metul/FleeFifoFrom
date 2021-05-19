55a253acdfc53948dc466735cad68e77355c5e14
Anastasia Pomelova <pomelova@in.tum.de>
2021-05-19 17:06:16 +0200

    button manager base interaction

--- FleeFifoFrom/Assets/Scripts/UI/ButtonManager.cs
+++ FleeFifoFrom/Assets/Scripts/UI/ButtonManager.cs
@@ -9,11 +9,14 @@ public class ButtonManager : MonoBehaviour
     // TODO replace with state manager
     private FieldManager _fieldManager;
 
-    [SerializeField] private Transform authorizeWorkerDisplay;
-    [SerializeField] private Transform swapWorkerDisplay;
-    [SerializeField] private Transform riotWorkerDisplay;
-    
-    
+    [SerializeField] private ActionTile _authorizeWorkerDisplay;
+    [SerializeField] private ActionTile _swapWorkerDisplay;
+    [SerializeField] private ActionTile _riotWorkerDisplay;
+    [SerializeField] private ActionTile _reviveWokerDisplay;
+    [SerializeField] private ActionTile _objectiveWorkerDisplay;
+
+    private ActionTile[] _actionTiles;
+
     // card & action tile states:
     // objective:   - draw card
     // countermand1 - draw card
@@ -23,20 +26,153 @@ public class ButtonManager : MonoBehaviour
     // recall:      - select action tile
     // cooperate:   - select one opponent worker
 
+    public enum DebugResetState
+    {
+        Default,
+        CountermandDrawCard,
+        CountermandSelectCard,
+        PoachSelectWorker,
+        PoachSelectCard,
+        Recall,
+        Cooperate
+    }
+
+    private DebugResetState _resetState;
+
+    public DebugResetState CurrentResetState
+    {
+        get => _resetState;
+        set
+        {
+            _resetState = value;
+            UpdateInteractability();
+        }
+    }
+
     private void Awake()
     {
         _fieldManager = FindObjectOfType<FieldManager>();
     }
 
+    private void Start()
+    {
+        _actionTiles = new[]
+        {
+            _authorizeWorkerDisplay,
+            _swapWorkerDisplay,
+            _riotWorkerDisplay,
+            _reviveWokerDisplay,
+            _objectiveWorkerDisplay
+        };
+    }
+
+    private void UpdateInteractability()
+    {
+        switch (CurrentResetState)
+        {
+            case DebugResetState.Default:
+                EnableActionTiles(false);
+                EnableWorkers(false);
+                EnableCardSelection(false);
+                break;
+            case DebugResetState.CountermandDrawCard:
+                EnableActionTiles(false);
+                EnableWorkers(false);
+                EnableCardSelection(false);
+                break;
+            case DebugResetState.CountermandSelectCard:
+                EnableActionTiles(false);
+                EnableWorkers(false);
+                EnableCardSelection(true);
+                break;
+            case DebugResetState.PoachSelectWorker:
+                EnableActionTiles(false);
+                EnableWorkers(true);
+                EnableCardSelection(false);
+                break;
+            case DebugResetState.PoachSelectCard:
+                EnableActionTiles(false);
+                EnableWorkers(false);
+                EnableCardSelection(true);
+                break;
+            case DebugResetState.Recall:
+                EnableActionTiles(true);
+                EnableWorkers(false);
+                EnableCardSelection(false);
+                break;
+            case DebugResetState.Cooperate:
+                EnableActionTiles(false);
+                EnableWorkers(true);
+                EnableCardSelection(false);
+                break;
+        }
+    }
+
+    private void EnableActionTiles(bool enabled)
+    {
+        foreach (var actionTile in _actionTiles)
+        {
+            actionTile.Interactable = enabled;
+        }
+    }
+
+    private void EnableWorkers(bool enabled)
+    {
+        // TODO only enable opponent worker
+        
+        foreach (var actionTile in _actionTiles)
+        {
+            actionTile.SetWorkerInteractable(enabled);
+        }
+    }
+
+    private void EnableCardSelection(bool enabled)
+    {
+        Debug.Log($"Enable Card Selection: {enabled}");
+    }
+
+
     public void OnActionTileClick(ActionTile actionTile)
     {
         Debug.Log("Action Tile has been clicked");
+        switch (CurrentResetState)
+        {
+            case DebugResetState.Recall:
+                var workers = actionTile.RemoveAllWorker();
+                foreach (var worker in workers)
+                {
+                    Debug.Log($"TODO: return worker {worker} to player {worker.PlayerId}");
+                }
+
+                CurrentResetState = DebugResetState.Default;
+                break;
+            default:
+                Debug.LogWarning("This is not a state in which action tile interaction is allowed!");
+            break;
+        }
     }
 
     public void OnWorkerClick(Worker worker)
     {
-        Debug.Log("Worker has been clicked");
+        switch (CurrentResetState)
+        {
+            case DebugResetState.Cooperate:
+                var w1 = worker.Tile.RemoveWorker(worker.ID);
+                Debug.Log($"TODO: return worker {worker} to player {worker.PlayerId}");
+                CurrentResetState = DebugResetState.Default;
+                break;
+            case DebugResetState.PoachSelectWorker:
+                var w2 = worker.Tile.RemoveWorker(worker.ID);
+                Debug.Log($"TODO: poach worker {worker} from player {worker.PlayerId}");
+                CurrentResetState = DebugResetState.PoachSelectCard;
+                break;
+            default:
+                Debug.LogWarning("This is not a state in which worker interaction is allowed!");
+                break;
+        }
     }
+
+    #region ui button onClicks
 
     public void Authorize()
     {
@@ -65,11 +201,13 @@ public class ButtonManager : MonoBehaviour
     public void Objective()
     {
         Debug.Log("Player chose the objective action");
+        // TODO
     }
 
     public void Countermand()
     {
         Debug.Log("Player chose the countermand action");
+        CurrentResetState = DebugResetState.CountermandDrawCard;
     }
 
     public void Reprioritize()
@@ -87,16 +225,19 @@ public class ButtonManager : MonoBehaviour
     public void Recall()
     {
         Debug.Log("Player chose the recall action");
+        CurrentResetState = DebugResetState.Recall;
     }
 
     public void Cooperate()
     {
         Debug.Log("Player chose the cooperate action");
+        CurrentResetState = DebugResetState.Cooperate;
     }
 
     public void Poach()
     {
         Debug.Log("Player chose the poach action");
+        CurrentResetState = DebugResetState.PoachSelectCard;
     }
 
     public void Villager()
@@ -115,4 +256,6 @@ public class ButtonManager : MonoBehaviour
     {
         Debug.Log("End Player Turn");
     }
+    
+    #endregion
 }
