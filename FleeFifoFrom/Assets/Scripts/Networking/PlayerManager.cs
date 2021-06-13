660e4bf0ff3db8b042083571dab6ae08271b9b80
metul <mertulker14@gmail.com>
2021-06-13 21:48:52 +0300

    Minor player count callback fix

--- FleeFifoFrom/Assets/Scripts/Networking/PlayerManager.cs
+++ FleeFifoFrom/Assets/Scripts/Networking/PlayerManager.cs
@@ -1,10 +1,9 @@
 using MLAPI;
+using MLAPI.Logging;
 using MLAPI.NetworkVariable;
 using MLAPI.NetworkVariable.Collections;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 /// <summary>
 /// Manager class for network players.
@@ -40,7 +39,8 @@ public class PlayerManager : NetworkBehaviour
 
     private NetworkVariableInt _playerCount = new NetworkVariableInt(new NetworkVariableSettings
     {
-        WritePermission = NetworkVariablePermission.ServerOnly
+        WritePermission = NetworkVariablePermission.ServerOnly,
+        SendTickrate = 0
     }, 0);
     public NetworkVariableInt PlayerCount => _playerCount;
 
@@ -67,13 +67,14 @@ public class PlayerManager : NetworkBehaviour
     /// <param name="next"> Player count after change. </param>
     private void OnPlayerCountChanged(int prev, int next)
     {
-        // Handle initial invocation
-        if (prev == next)
+        if (IsServer || prev == next)
             return;
         // Map client ID to player ID
         if (!_networkPlayerIDs.ContainsKey(NetworkManager.Singleton.LocalClientId))
         {
             DPlayer.ID playerID = GameState.Instance.Players[PlayerCount.Value - 1].Id;
+            NetworkLog.LogInfoServer($"{prev}/{next}: mapping client id {NetworkManager.Singleton.LocalClientId} to player id {playerID}");
+            // TODO: Fix bug where callback is unnecessarily executed on networkvariable init (e.g. 0->2, then 2->3)
             _networkPlayerIDs.Add(NetworkManager.Singleton.LocalClientId, playerID);
             // Set player indicator color on UI
             ConnectionManager.Instance.SetPlayerIndicatorColor(ColorUtils.GetPlayerColor(playerID));
@@ -81,6 +82,7 @@ public class PlayerManager : NetworkBehaviour
         // Update text
         int requiredPlayers = GameState.Instance.Players.Length;
         ConnectionManager.Instance.ModifyWaitingText(PlayerCount.Value, requiredPlayers);
+        // TODO: Start/Stop game on server as well?
         // Initialize/interrupt game
         if (PlayerCount.Value == requiredPlayers)
             StartCoroutine(StartGame());
