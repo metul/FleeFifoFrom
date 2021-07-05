f7cbf9138747e7c5259b1a493bdd62758f6d82aa
Anastasia Pomelova <pomelova@in.tum.de>
2021-07-05 14:06:06 +0200

    Basic main menu

---
+++ FleeFifoFrom/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    private const int LOCAL_SCENE_INDEX = 1;
+    private const int ONLINE_SCENE_INDEX = 2;
+    
+    public void LoadLocalGame()
+    {
+        GameState.LocalGame = true;
+        SceneManager.LoadScene(LOCAL_SCENE_INDEX);
+    }
+    
+    public void LoadOnlineGame()
+    {
+        GameState.LocalGame = false;
+        SceneManager.LoadScene(ONLINE_SCENE_INDEX);
+    }
+
+    public void ChangePlayerCount(int val)
+    {
+        GameState.PlayerCount = Rules.MAX_PLAYER_COUNT - val;
+    }
+}
--- FleeFifoFrom/Assets/Scripts/State/Rules.cs
+++ FleeFifoFrom/Assets/Scripts/State/Rules.cs
@@ -17,6 +17,8 @@ public class Rules
     { DPlayer.ID.Green, new DPosition(4, 3) }
   };
 
+  public const int MAX_PLAYER_COUNT = 4;
+
   public static readonly short[] HONOR_VALUES = { -15, -12, -9, -6, -3, -1, 0, 1, 3, 6, 9, 12, 15 };
   
   public const ushort WORKER_COUNT = 2;
